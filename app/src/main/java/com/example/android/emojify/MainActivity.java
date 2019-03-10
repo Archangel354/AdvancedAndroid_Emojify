78091e5e9696c7273e7a6b5c3b432af15529ebaf
Archangel354 <rduncan354@gmail.com>
2019-03-10 16:34:39 -0400

    Exercise 5

--- app/src/main/java/com/example/android/emojify/MainActivity.java
+++ app/src/main/java/com/example/android/emojify/MainActivity.java
@@ -180,9 +180,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Resample the saved image to fit the ImageView
         mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
-        
-        // Detect the faces
-        Emojifier.detectFaces(this, mResultsBitmap);
+
+        // Detect the faces and overlay the appropriate emoji
+        mResultsBitmap = Emojifier.detectFacesandOverlayEmoji(this, mResultsBitmap);
         // TODO (10): Change the method call from detectFaces() to detectFacesAndOverlayEmoji() and assign the result to mResultsBitmap.
         
         // Set the new bitmap to the ImageView
